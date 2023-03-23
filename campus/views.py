df24925b6859c94b905bd19964317d1514f7baa0
Santiago <santiago@cyberpuerta.mx>
2023-03-23 13:20:21 -0600

    teachers

--- campus/views.py
+++ campus/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponseRedirect, HttpResponse
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.db import connection
 from django.urls import reverse
@@ -8,32 +8,35 @@ def index(request):
     return render(request, 'campus/home.html')
 
 
+# region: students
+
 def students_index(request):
     with connection.cursor() as cursor:
-        cursor.execute("SELECT * FROM students")
+        cursor.execute("""
+            SELECT id, name, last_name, date_of_birth, favorite_number, country_of_origin, active, created_at
+            FROM students""")
         students = dict_fetchall(cursor)
     return render(request, 'campus/students_index.html', {'students': students})
 
 
 def students_detail(request, student_id):
     with connection.cursor() as cursor:
-        q = "SELECT * FROM students WHERE id = %s"
+        q = """
+            SELECT id, name, last_name, date_of_birth, favorite_number, country_of_origin, active, created_at
+            FROM students
+            WHERE id = %s"""
         cursor.execute(q, [student_id])
         student = dict_fetchone(cursor)
     return render(request, 'campus/students_detail.html', {'student': student, 'student_id': student_id})
 
 
 def students_add(request):
-    with connection.cursor() as cursor:
-        q = "SELECT * FROM students LIMIT 1"  # any, just to get the columns
-        cursor.execute(q)
-        student = dict_fetchone(cursor)
-        student = {k: '' for k in student}  # no values please
-    return render(request, 'campus/students_detail.html', {'student': student, 'student_id': 0})
+    student = dict(id='', name='', last_name='', date_of_birth='', favorite_number='', country_of_origin='',
+                   active='', created_at='')
+    return render(request, 'campus/students_detail.html', {'student': student, 'create': True})
 
 
 def students_save(request, student_id):
-    the_id = request.POST['student_id'] if student_id == 0 else student_id
     posted = [
         request.POST['name'],
         request.POST['last_name'],
@@ -41,27 +44,94 @@ def students_save(request, student_id):
         request.POST['favorite_number'],
         request.POST['country_of_origin'],
         1 if 'active' in request.POST else 0,
-        request.POST['student_id'] if student_id == 0 else student_id,
+        student_id,
     ]
     with connection.cursor() as cursor:
-        if student_id == 0:  # zero means new student
-            q = """INSERT INTO students (name, last_name, date_of_birth, favorite_number, country_of_origin, active, id)
-                   VALUES (%s, %s, %s, %s, %s, %s, %s)
-                """
-        else:
-            q = """UPDATE students SET
-                name=%s,
-                last_name=%s,
-                date_of_birth=%s,
-                favorite_number=%s,
-                country_of_origin=%s,
-                active=%s
-                WHERE id=%s
+        q = """
+            UPDATE students
+            SET name=%s, last_name=%s, date_of_birth=%s, favorite_number=%s, country_of_origin=%s, active=%s
+            WHERE id=%s"""
+        cursor.execute(q, posted)
+        connection.commit()
+        return HttpResponseRedirect(reverse('campus:students-detail', args=(student_id,)))
+
+
+def students_create(request):
+    posted = [
+        request.POST['name'],
+        request.POST['last_name'],
+        request.POST['date_of_birth'],
+        request.POST['favorite_number'],
+        request.POST['country_of_origin'],
+        1 if 'active' in request.POST else 0,
+        request.POST['id'],
+    ]
+    with connection.cursor() as cursor:
+        q = """
+            INSERT INTO students (name, last_name, date_of_birth, favorite_number, country_of_origin, active, id)
+            VALUES (%s, %s, %s, %s, %s, %s, %s)"""
+        cursor.execute(q, posted)
+        connection.commit()
+        return HttpResponseRedirect(reverse('campus:students-detail', args=(request.POST['id'],)))
+
+# endregion
+
+
+# region: teachers
+
+def teachers_index(request):
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT id, name, last_name, date_of_birth, degree, created_at FROM teachers")
+        teachers = dict_fetchall(cursor)
+    return render(request, 'campus/teachers_index.html', {'teachers': teachers})
+
+
+def teachers_detail(request, teacher_id):
+    with connection.cursor() as cursor:
+        q = "SELECT id, name, last_name, date_of_birth, degree, created_at FROM teachers WHERE id = %s"
+        cursor.execute(q, [teacher_id])
+        teacher = dict_fetchone(cursor)
+    return render(request, 'campus/teachers_detail.html', {'teacher': teacher, 'teacher_id': teacher_id})
+
+
+def teachers_add(request):
+    teacher = dict(id='', name='', last_name='', date_of_birth='', degree='', created_at='')
+    return render(request, 'campus/teachers_detail.html', {'teacher': teacher, 'create': True})
+
+
+def teachers_save(request, teacher_id):
+    posted = [
+        request.POST['name'],
+        request.POST['last_name'],
+        request.POST['date_of_birth'],
+        request.POST['degree'],
+        teacher_id,
+    ]
+    with connection.cursor() as cursor:
+        q = "UPDATE teachers SET name=%s, last_name=%s, date_of_birth=%s, degree=%s WHERE id=%s"
+        cursor.execute(q, posted)
+        connection.commit()
+        return HttpResponseRedirect(reverse('campus:teachers-detail', args=(teacher_id,)))
+
+
+def teachers_create(request):
+    posted = [
+        request.POST['name'],
+        request.POST['last_name'],
+        request.POST['date_of_birth'],
+        request.POST['degree'],
+        request.POST['id'],
+    ]
+    with connection.cursor() as cursor:
+        q = """INSERT INTO teachers (name, last_name, date_of_birth, degree, id)
+               VALUES (%s, %s, %s, %s, %s)
             """
         cursor.execute(q, posted)
         connection.commit()
-        return HttpResponseRedirect(reverse('campus:students-detail', args=(the_id,)))
+        return HttpResponseRedirect(reverse('campus:teachers-detail', args=(request.POST['id'],)))
 
+
+# region: reports
 
 def student_enrollments(request):
     query = """
@@ -92,8 +162,10 @@ def student_enrollments(request):
 
     return render(request, 'campus/student_enrollments.html', context)
 
+# endregion
 
-# move to another file:
+
+# region: utils (move to another file)
 
 def dict_fetchall(cursor):
     """Return all rows from a cursor as a list of dicts"""
@@ -106,3 +178,5 @@ def dict_fetchone(cursor):
     columns = [col[0] for col in cursor.description]
     row = cursor.fetchone()
     return dict(zip(columns, row))
+
+# endregion
